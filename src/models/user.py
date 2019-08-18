d838541b547d0da342b0263d4c64b33a8c7e3da2
Vladislav <filonich.v@mail.ru>
2019-08-18 16:00:44 +0300

    Created User model.

---
+++ src/models/user.py
@@ -0,0 +1,68 @@
+from uuid import uuid4
+
+from flask import session
+
+from src.common.database import Database
+from src.models.blog import Blog
+
+
+class User:
+    def __init__(self, email, password, _id=None):
+        self.email = email
+        self.password = password
+        self._id = uuid4().hex if _id is None else _id
+
+    @classmethod
+    def get_by_email(cls, email):
+        data = Database.find_one(collection='users',
+                                 query={'email': email})
+        if data is not None:
+            return cls(**data)
+
+    @classmethod
+    def get_by_id(cls, _id):
+        data = Database.find_one(collection='users',
+                                 query={'_id': _id})
+        if data is not None:
+            return cls(**data)
+
+    @staticmethod
+    def login_valid(email, password):
+        user = User.get_by_email(email)
+        if user is not None:
+            return user.password == password
+        return False
+
+    @classmethod
+    def register(cls, email, password):
+        user = cls.get_by_email(email)
+        if user is None:
+            new_user = cls(email, password)
+            new_user.save_to_mongo()
+            session['email'] = email
+            return True
+        else:
+            # Show message that we could't register user and why.
+            return False
+
+    @staticmethod
+    def login(user_email):
+        # login valid has already been called
+        session['email'] = user_email
+
+    @staticmethod
+    def logout():
+        session['email'] = None
+
+    def get_blogs(self):
+        return Blog.find_by_author_id(self._id)
+
+    def json(self):
+        return {
+            'email': self.email,
+            '_id': self._id,
+            'password': self.password
+        }
+
+    def save_to_mongo(self):
+        Database.insert(collection='users', data=self.json())
