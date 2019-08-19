7af54723da8456963c55949c07ed01e0b67ad49b
Vladislav <filonich.v@mail.ru>
2019-08-19 21:08:23 +0300

    Applying a little of Bootstrap 4.

--- src/app.py
+++ src/app.py
@@ -8,8 +8,18 @@ app.secret_key = "123"
 
 
 @app.route('/')
-def hello_method():
+def home_template():
+    return render_template('home.html')
+
+
+@app.route('/login')    # www.mysite.com/api/login
+def login_template():
     return render_template('login.html')
+
+
+@app.route('/register')    # www.mysite.com/api/register
+def register_template():
+    return render_template('register.html')
 
 
 @app.before_first_request
@@ -17,15 +27,35 @@ def initialize_database():
     Database.initialize()
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
 
     if User.login_valid(email=email, password=password):
         User.login(user_email=email)
+    else:
+        session['email'] = None
 
     return render_template("profile.html", email=session['email'])
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email=email, password=password)
+
+    return render_template('profile.html', email=session['email'])
+
+
+@app.route('blogs/<string:user_id>')
+def user_blogs(user_id):
+    user = User.get_by_id(user_id)
+    user.get_blogs()
+
+    render_template("user_blogs.html", blogs=user_blogs)
 
 
 if __name__ == '__main__':
