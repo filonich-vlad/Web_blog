f40e76d77dc0f6e982a9e4417a2bb6cf6544f408
Vladislav <filonich.v@mail.ru>
2019-08-19 16:24:45 +0300

    Login successful.

--- src/app.py
+++ src/app.py
@@ -1,11 +1,31 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
+
+from src.common.database import Database
+from src.models.user import User
 
 app = Flask(__name__)
+app.secret_key = "123"
 
 
 @app.route('/')
 def hello_method():
     return render_template('login.html')
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email=email, password=password):
+        User.login(user_email=email)
+
+    return render_template("profile.html", email=session['email'])
 
 
 if __name__ == '__main__':
